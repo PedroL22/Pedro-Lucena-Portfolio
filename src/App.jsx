451c34f4b7c811872f59313b028437a858a18b95
Pedro L <pedrocontact22@gmail.com>
2022-08-29 22:03:49 -0300

    Fixed Section1 height

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import './App.css';
 import NavBar from './components/NavBar/NavBar';
 import Section1 from './components/Section1/Section1';
 import Section2 from './components/Section2/Section2';
+import Section3 from './components/Section3/Section3';
 
 function App() {
   const [darkHeader, setDarkHeader] = useState(false);
@@ -28,6 +29,7 @@ function App() {
       <NavBar dark={darkHeader} />
       <Section1 />
       <Section2 />
+      <Section3 />
     </div>
   );
 }
---
+++ src/components/Section3/Section3.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import './styles.css';
+
+function Section3() {
+  return <div className="sla">teste</div>;
+}
+
+export default Section3;
