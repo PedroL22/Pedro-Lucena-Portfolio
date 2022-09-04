5b494d08e1fd15f625dc2c415b4673b6fd64d535
Pedro L <pedrocontact22@gmail.com>
2022-09-04 20:41:22 -0300

    Footer

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import './App.css';
+import Footer from './components/Footer/Footer';
 import NavBar from './components/NavBar/NavBar';
 import Section1 from './components/Section1/Section1';
 import Section2 from './components/Section2/Section2';
@@ -32,6 +33,7 @@ function App() {
       <Section2 />
       <Section3 />
       <Section4 />
+      <Footer />
     </div>
   );
 }
