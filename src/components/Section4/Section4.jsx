209a597c652370e4ddedb2d2a9918e664f6440d3
Pedro L <pedrocontact22@gmail.com>
2022-09-05 14:34:34 -0300

    Success Message for Section4 form

--- src/components/Section4/Section4.jsx
+++ src/components/Section4/Section4.jsx
@@ -1,26 +1,32 @@
-import React, { useRef } from 'react';
+import React, { useState, useRef } from 'react';
 import './styles.css';
 import emailjs from 'emailjs-com';
 import PersonalText from '../../assets/images/undraw_personal_text.svg';
 
 function Section4() {
+  const [message, setMessage] = useState(false);
   const form = useRef();
   const sendEmail = (e) => {
     e.preventDefault();
 
-    emailjs.sendForm('service_p8f68rz', 'template_ixeh0fe', form.current, '-xo0bQBMsXWq4CwN9').then(
-      (result) => {
-        console.log(result.text);
-      },
-      (error) => {
-        console.log(error.text);
-      },
-    );
+    emailjs
+      .sendForm('service_p8f68rz', 'template_ixeh0fe', form.current, '-xo0bQBMsXWq4CwN9')
+      .then();
     e.target.reset();
+    setMessage(
+      setTimeout(() => {
+        setMessage(false);
+      }, 3000),
+    );
   };
 
   return (
     <div className="section4">
+      {message && (
+        <div className="success-message">
+          <h3>Message sent successfully.</h3>
+        </div>
+      )}
       <div className="form-wrapper">
         <div className="left">
           <img src={PersonalText} alt="" />
