80858d195fd75ea6a1d6e9fa008f2d009fd055eb
Pedro L <pedrocontact22@gmail.com>
2022-09-19 13:49:18 -0300

    Added open in new tab to projects links

--- src/components/Section3/Project/Project.jsx
+++ src/components/Section3/Project/Project.jsx
@@ -10,10 +10,10 @@ function Project({
         <img className="image" src={image} />
       </div>
       <h3 className="name">{name}</h3>
-      <a className="github" href={github}>
+      <a className="github" href={github} target="_blank" rel="noreferrer">
         gitHub
       </a>
-      <a className="live" href={live}>
+      <a className="live" href={live} target="_blank" rel="noreferrer">
         Live demo
       </a>
     </div>
