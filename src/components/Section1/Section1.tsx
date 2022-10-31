9df86feb2cf14303e53aa737ac508ef8c155aba7
Pedro L <pedrocontact22@gmail.com>
2022-10-31 13:36:07 -0300

    Section 1 changes

--- src/components/Section1/Section1.tsx
+++ src/components/Section1/Section1.tsx
@@ -1,14 +1,14 @@
-import React from 'react';
-import './styles.css';
-import { animateScroll } from 'react-scroll';
-import Typed from 'react-typed';
-import rightArrow from '../../assets/images/right-arrow.png';
+import React from "react";
+import "./styles.css";
+import { animateScroll } from "react-scroll";
+import Typed from "react-typed";
+import rightArrow from "../../assets/images/right-arrow.png";
 
 function Section1() {
   const handleClick2 = (event) => {
     animateScroll.scrollTo(5000, { smooth: true, duration: 500 });
-    setActive('header-right');
-    setToggleIcon('hamburger');
+    setActive("header-right");
+    setToggleIcon("hamburger");
   };
 
   return (
@@ -16,25 +16,21 @@ function Section1() {
       <div className="text">
         <h2 className="welcome">Welcome to my Portfolio</h2>
         <h1>
-          Hi! I'm Pedro Lucena
-          {' '}
-          <br />
-          {' '}
+          Hi! I'm Pedro Lucena <br />{" "}
           <Typed
-            strings={['Full Stack', 'React', 'Django']}
+            strings={["Full Stack", "Front-End", "Back-End", "React", "Node"]}
             typeSpeed={50}
             backSpeed={60}
             loop
             className="typed"
           />
-          <br className="breakline" />
-          {' '}
-          Developer
+          <br className="breakline" /> Developer
         </h1>
         <p>
-          I'm a Full Stack Developer capable of creating useful applications with React JS and
-          Django Framework, I have a lot of experience with HTML, CSS and Bootstrap. I've worked
-          with Amazon Web Services and Heroku.
+          I'm a Full Stack Developer capable of creating useful applications
+          with React JS and Django Framework. I have a lot of experience with
+          HTML, CSS and Bootstrap. I've worked with Amazon Web Services and
+          Heroku.
         </p>
         <div className="connect" onClick={handleClick2}>
           <h2 className="connect-text">Let's connect!</h2>
