52ee8e245da0bd3f92d85da49152058f2f230412
Pedro L <pedrocontact22@gmail.com>
2022-12-07 16:41:46 -0300

    Update Section3.jsx

--- src/components/Section3/Section3.jsx
+++ src/components/Section3/Section3.jsx
@@ -12,7 +12,7 @@ function Section3() {
           name="Next JS Social Network"
           skills="NextJS Prisma Tailwind Daisy UI"
           projectdesc="A Next JS social network prototype built with Tailwind CSS, Daisy UI and Prisma."
-          image="https://user-images.githubusercontent.com/40894497/206275578-af670bc6-09be-45a0-9d8e-bab37bb1581d.jpg"
+          image="https://user-images.githubusercontent.com/40894497/206275586-7bd956a7-c2a5-478e-bae0-cb0ae098a1d9.jpg"
           github="https://github.com/PedroL22/Next-JS-Social-Network"
           live="http://next-js-social-network.vercel.app/"
         />
