e1f68591b9719054cf10b13caffcaa931fa7dd65
Pedro Lucena <pedrocontact22@gmail.com>
2023-11-21 08:45:51 -0300

    📦 chore: Upgrade packages

--- app/page.tsx
+++ app/page.tsx
@@ -5,7 +5,7 @@ export default function Home() {
     <main className='flex w-full flex-col items-center pt-24 text-gray-50'>
       <section
         aria-label='Home - Welcome to my portfolio'
-        className='flex h-[75vh] max-h-[900px] w-full max-w-screen-2xl flex-col justify-center bg-red-500 px-12'
+        className='flex h-[75vh] max-h-[900px] w-full max-w-screen-2xl flex-col justify-center px-12'
       >
         <div className='flex w-full flex-col items-center space-y-5 md:w-3/4 md:items-start'>
           <h2 className='w-fit border-2 border-secondaryDark bg-gradient-to-r from-secondaryDark to-primary p-2 text-xl font-medium md:p-3 md:text-2xl'>
